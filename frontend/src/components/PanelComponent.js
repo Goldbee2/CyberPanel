8395d5c90b64b53555c473b84901fc20e9b34814
Eli Goldberg <goldbee2@wwu.edu>
2022-10-05 23:57:17 -0700

    Created skeleton for panel component

---
+++ frontend/src/components/PanelComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+
+function PanelComponent({componentFunction, themeInfo}){
+    const panelHeight = 0;
+    const panelWidth = 0;
+
+
+    // basically: try to run function passed to it and contain it in nicely formatted panel according to theme.
+    // ideally components should take in theme info as maybe a name or set of css styles or pointer to a css file? and then output rendered component accordingly. 
+
+    return null;
+}
