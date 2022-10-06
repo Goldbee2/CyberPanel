1b635b127e42600b1a91645e1a283c30647c1636
Eli Goldberg <goldbee2@wwu.edu>
2022-10-05 20:29:04 -0700

    stylized welcome message

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Want to know what this does? Go to http://github.com/goldbee2/CyberPanel.' });
+router.get("/", function (req, res, next) {
+  res.render("index", {
+    title: "Want to know what this does?",
+    description: "Go to http://github.com/goldbee2/CyberPanel.",
+  });
 });
 
 module.exports = router;
