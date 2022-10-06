5889da07578dcece63593085397e9b95db2038fa
Eli Goldberg <goldbee2@wwu.edu>
2022-10-05 20:17:20 -0700

    Added placeholder message with repo link

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Want to know what this does? Go to http://github.com/goldbee2/CyberPanel.' });
 });
 
 module.exports = router;
