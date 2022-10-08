db9f2edcb348981d7cee09c5009f1d344c7ec9b0
Eli Goldberg <goldbee2@wwu.edu>
2022-10-08 16:10:29 -0700

    Split and improved weather and geocoding fetch implementations

--- backend/getWeather.js
+++ backend/getWeather.js
@@ -1,21 +1,51 @@
 var apiKeys = require("./apiKeys");
 
-// call geocoding API
 
-function getWeather() {
+
+/**
+ * Summary.                  Fetches geocoding data from openWeather API.
+ *
+ * Description.             Async function which makes GET request to openWeather API. Response will be an object.
+ *                          If errored, object contains two fields (error code and error message). If sucessful,
+ *                          object contains 4 fields: zip, name, lat, lon, and country.
+ *
+ * @param {*} zipCode       ZIP code for desired location
+ * @param {*} countryCode   ISO 3166 country code for desired location ('US' for the United States)
+ * @param {*} apiKey        API key for OpenWeather. For security practices, should be stored in APIKeys.env
+ * @returns
+ */
+
+async function fetchGeocodingData(zipCode, countryCode, apiKey) {
+  let url = `http://api.openweathermap.org/geo/1.0/zip?zip=${zipCode},${countryCode}&appid=${apiKey}`;
+
+  return fetch(url).then((response) => {
+    if (response.ok) {
+      return response.json().then((response) => ({ response }));
+    }
+    return response.json().then((error) => ({ error }));
+  });
+}
+
+
+
+
+
+async function fetchWeatherData() {
   let personalInfo = require("./personalInfo").parsedFields;
-
+  let key = require("./apiKeys").getKey("weather");
   let zipCode = personalInfo.ZIP_CODE;
   let countryCode = personalInfo.COUNTRY_CODE;
 
-  const getGeocodingData = async (zipCode) => {
-    let key = apiKeys.getKey("weather");
-    let url = `http://api.openweathermap.org/geo/1.0/zip?zip=${zipCode},${countryCode}&appid=${key}`;
-    const res = await fetch(url);
-    const data = await res.json();
-    return data.json;
-  };
-  let data = getGeocodingData(zipCode).then(console.log(data));
+  const { response, error } = await fetchGeocodingData(
+    zipCode,
+    countryCode,
+    key
+  );
+  if (response) {
+    console.log("Response: ", response);
+  } else {
+    console.log("Error:", error);
+  }
 }
-console.log(getWeather());
-getWeather();
+
+fetchWeatherData();
