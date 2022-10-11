5c1f987588e9baffd5e37d9cedf52be901f43213
Eli Goldberg <goldbee2@wwu.edu>
2022-10-10 20:50:58 -0700

    added CORS

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors = require('cors');
 
 // Routers
 var indexRouter = require('./routes/index');
@@ -13,6 +13,7 @@ const { getSystemErrorMap } = require('util');
 
 
 var app = express();
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
