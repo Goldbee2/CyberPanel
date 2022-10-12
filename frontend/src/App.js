cc894d64adea8c0aab0a7bd05147ac61462bf8d9
Eli Goldberg <goldbee2@wwu.edu>
2022-10-11 21:06:31 -0700

    Added clock component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import PanelComponent from "./components/PanelComponent";
 import WeatherComponent from "./components/WeatherComponent";
 import { useEffect, useState } from "react";
+import ClockComponent from "./components/ClockComponent";
 
 function App() {
   return (
     <div className="App">
-      <WeatherComponent />
+      <WeatherComponent/>
+      <ClockComponent/>
     </div>
   );
 }
