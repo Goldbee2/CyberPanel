6280df5fceac067c35c94753a5059e7f706eb8e8
Eli Goldberg <goldbee2@wwu.edu>
2022-10-14 21:55:12 -0700

    Added lights component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import PanelComponent from "./components/PanelComponent";
 import WeatherComponent from "./components/WeatherComponent";
 import { useEffect, useState } from "react";
 import ClockComponent from "./components/ClockComponent";
+import LightsComponent from "./components/LightsComponent";
 
 function App() {
   return (
     <div className="App">
       <WeatherComponent/>
       <ClockComponent/>
+      <LightsComponent/>
     </div>
   );
 }
