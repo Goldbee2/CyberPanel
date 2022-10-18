1b53a1b6d27f66d92558f2b12e1d98cb207739ec
Eli Goldberg <goldbee2@wwu.edu>
2022-10-18 01:31:35 -0700

    Initialized + noted OAuth2 flow in comment

---
+++ backend/oAuth2.js
@@ -0,0 +1,12 @@
+function getAuthorization(){
+
+    //<===============FLOW=============>
+    //  
+    //  Based on https://developers.google.com/identity/protocols/oauth2
+    // 
+    // 1. Get token from files
+    // 2. Request user credentials and use them to get access token from google server
+    // 3. Examine scopes granted by user
+    // 4. Send access token to API
+    // 5. Refresh token if necessary
+}
