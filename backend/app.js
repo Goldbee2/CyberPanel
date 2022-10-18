3afae6b0b06f196d0ca1aab8b49f0698d0c33798
Eli Goldberg <goldbee2@wwu.edu>
2022-10-18 01:33:02 -0700

    commented example pattern for global var instantiation

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,10 @@ app.use(cors());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
+// Setting globals
+// app.set("example", exampleModule.exampleData());
+
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
