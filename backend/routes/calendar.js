7d98bb76f7ceeeee0fcef02a5551a57ceff90042
Eli Goldberg <goldbee2@wwu.edu>
2022-10-18 01:29:22 -0700

    Created js files for calendar component

---
+++ backend/routes/calendar.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var router = express.Router();
+/*
+router.get("/", function (req, res, next) {
+  let a = calendarFunctions.getEvents();
+
+  a.then((b) => {
+    console.log(b);
+    res.send(b);
+  });
+  res.setHeader("content-type", "application/json");
+});
+*/
+
+module.exports = router;
---
+++ frontend/src/components/CalendarComponent.js
@@ -0,0 +1,5 @@
+import react from 'react';
+
+function calendarComponent(){
+    
+}
