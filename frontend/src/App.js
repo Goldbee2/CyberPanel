42d5fff76f88cc2300d9038b0bbc027d35bdf0a4
Eli Goldberg <goldbee2@wwu.edu>
2023-01-20 16:49:53 -0800

    Added new styled components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,16 @@ import WeatherComponent from "./components/WeatherComponent";
 import { useEffect, useState } from "react";
 import ClockComponent from "./components/ClockComponent";
 import LightsComponent from "./components/LightsComponent";
+import ClockWeatherComponent from "./components/ClockWeatherComponent";
+import WeatherPanelComponent from "./components/WeatherPanelComponent";
 
 function App() {
   return (
     <div className="App">
-      <WeatherComponent/>
+      <WeatherPanelComponent/>
       <ClockComponent/>
       <LightsComponent/>
+      <ClockWeatherComponent/>
     </div>
   );
 }
