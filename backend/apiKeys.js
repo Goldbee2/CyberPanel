f9418630790747a5fff18c41f881d76b12487f67
Eli Goldberg <goldbee2@wwu.edu>
2023-02-10 15:07:22 -0800

    added oauth2 keys and sassy comments

--- backend/apiKeys.js
+++ backend/apiKeys.js
@@ -1,4 +1,8 @@
 // Singleton
+// TODO: refactor the whole env keyring function, don't think the singleton pattern is needed
+// TODO: research key in memory best practices, want to make reduce vulnerabilities
+
+
 var ApiKeys = function () {
   var instance;
 
@@ -19,6 +23,9 @@ var ApiKeys = function () {
   return getInstance();
 };
 
+
+// Hideous function that evolved over time to be disgusting, need to refactor. Probably a good design pattern for this one?
+
 function getKey(apiName) {
   var parsedKeys = ApiKeys();
   switch (apiName) {
@@ -36,10 +43,36 @@ function getKey(apiName) {
         console.log(err.message);
         return "ERROR_KEY_NOT_FOUND";
       }
+     case "oauth2ClientID":
+      try{
+        return parsedKeys.OAUTH2_CLIENT_ID;
+      }catch(err){
+        console.log(err.message);
+        return "ERROR_KEY_NOT_FOUND";
+      }
+
+      case "oauth2ClientSecret":
+      try{
+        return parsedKeys.OAUTH2_CLIENT_SECRET;
+      }catch(err){
+        console.log(err.message);
+        return "ERROR_KEY_NOT_FOUND";
+      }
+
+      case "oauth2RedirectURL":
+      try{
+        return parsedKeys.OAUTH2_REDIRECT_URL;
+      }catch(err){
+        console.log(err.message);
+        return "ERROR_KEY_NOT_FOUND";
+      }
+
+
     default:
       console.log("Invalid api name passed to getKey: " + apiName);
       return "";
   }
 }
+
 
 module.exports = { getKey };
