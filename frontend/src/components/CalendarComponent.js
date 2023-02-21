c727d5c1af65451fac6ea61fa4c2c6299389c322
Eli Goldberg <goldbee2@wwu.edu>
2023-02-20 21:32:06 -0800

    Commented needed functionality

--- frontend/src/components/CalendarComponent.js
+++ frontend/src/components/CalendarComponent.js
@@ -1,5 +1,10 @@
 import react from 'react';
 
 function calendarComponent(){
+    //states:
+        //Click to sign in
+        //Sign-in process
+        //Display calendar
+        //Errored
     
 }
