6755f9cc5d5b41ff0014a6c6f0a658e64d987f4c
Eli Goldberg <goldbee2@wwu.edu>
2023-02-20 21:30:35 -0800

    Added auth redirect code skeleton

---
+++ frontend/src/AuthRedirect.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+
+// "Needs: success state, failed state, store cookies on success, prompt user to close window."
+
+
+export default function AuthRedirect(){
+    
+}
