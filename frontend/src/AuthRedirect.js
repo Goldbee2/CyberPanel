8182b4fa1182ca04652c0cc0b9152cd458f8107c
Eli Goldberg <goldbee2@wwu.edu>
2023-02-22 17:12:26 -0800

    Saves auth token as cookie on redirect

--- frontend/src/AuthRedirect.js
+++ frontend/src/AuthRedirect.js
@@ -1,10 +1,13 @@
 import React from "react";
-
+import Cookies from 'universal-cookie';
 
 
 // "Needs: success state, failed state, store cookies on success, prompt user to close window."
 
 
 export default function AuthRedirect(){
-    
+    const queryParameters = new URLSearchParams(window.location.search);
+    const cookies = new Cookies();
+    cookies.set('googleAuthToken', queryParameters.get('code'), {secure:true, sameSite:"Strict"});
+    console.log(queryParameters);
 }
