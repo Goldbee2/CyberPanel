ca209cde5c11c9530b9640f0f9518760aa89f161
Eli Goldberg <goldbee2@wwu.edu>
2023-02-22 17:13:08 -0800

    implemented routing + error handling

--- backend/routes/calendar.js
+++ backend/routes/calendar.js
@@ -1,15 +1,26 @@
 var express = require("express");
 var router = express.Router();
-/*
-router.get("/", function (req, res, next) {
-  let a = calendarFunctions.getEvents();
 
-  a.then((b) => {
-    console.log(b);
-    res.send(b);
-  });
-  res.setHeader("content-type", "application/json");
+router.get("/", function (req, res, next) {
+  const token = req.cookies.googleAuthToken;
+  if (token) {
+    let((retrievedCalEvents = calendarFunctions.getEvents()));
+
+    retrievedCalEvents.then((calendarEventsResponse) => {
+      console.log(calendarEventsResponse);
+      res.send(calendarEventsResponse);
+    });
+  } else {
+    res.status(401);
+    res.send("No token included with request");
+  }
+  // let a = calendarFunctions.getEvents();
+
+  // a.then((b) => {
+  //   console.log(b);
+  //   res.send(b);
+  // });
+  // res.setHeader("content-type", "application/json");
 });
-*/
 
 module.exports = router;
