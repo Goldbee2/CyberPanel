3affba67625c88843e2c273df9a1ac5563139673
Eli Goldberg <goldbee2@wwu.edu>
2023-03-07 18:35:43 -0800

    Added redirect link, changed cookie name

--- frontend/src/AuthRedirect.js
+++ frontend/src/AuthRedirect.js
@@ -8,6 +8,8 @@ import Cookies from 'universal-cookie';
 export default function AuthRedirect(){
     const queryParameters = new URLSearchParams(window.location.search);
     const cookies = new Cookies();
-    cookies.set('googleAuthToken', queryParameters.get('code'), {secure:true, sameSite:"Strict"});
+    cookies.set('googleAuthToken', queryParameters.get('googleAuthToken'), {secure:true, sameSite:"Strict"});
     console.log(queryParameters);
+
+    return(<p>This page should redirect you. If it doesn't, click <a href="https://localhost:3000">here</a> to be redirected.</p>)
 }
