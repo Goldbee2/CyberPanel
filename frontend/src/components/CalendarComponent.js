61269feae8aadd4c2b3dd4fe66f46fd15b8b15f1
Eli Goldberg <goldbee2@wwu.edu>
2023-03-07 18:35:11 -0800

    Updated component state flow and api calls

--- frontend/src/components/CalendarComponent.js
+++ frontend/src/components/CalendarComponent.js
@@ -1,67 +1,89 @@
-import react from 'react';
-import PanelComponent from './PanelComponent';
-import AuthPrompt from './AuthPrompt';
+import React, { useState, useEffect } from "react";
+import PanelComponent from "./PanelComponent";
+import AuthPrompt from "./AuthPrompt";
+import Error from "./Error";
+import Cookies from "universal-cookie";
 
-function calendarComponent(){
-    //states:
-        //Click to sign in
-        //Sign-in process
-        //Display calendar
-        //Errored
-    const [eventData, setEventData] = useState({});
-    const [componentState, setComponentState] = useState("loading");
-    var authURL = "";
+export default function CalendarComponent() {
+  //states:
+  //Click to sign in
+  //Sign-in process
+  //Display calendar
+  //Errored
+  const [calendarData, setCalendarData] = useState({});
+  const [componentState, setComponentState] = useState("loading");
+  const [authURL, setAuthURL] = useState("");
 
-    useEffect(()=>{
-        setComponentState("loading");
+  useEffect(() => {
+    const cookies = new Cookies();
 
-        if(true /*cookie exists*/){
-             fetch("http://192.168.1.127:9000/calendar") //with auth token in req
-                .then((res)=>{
-                    setEventData(res);
-                    setComponentState("displayEvents");
-                })
-                .catch((err)=>{
-                    console.error("Error:", err);
-                    setComponentState("error");
-                });
-         } else {
-            fetch("http://192.168.1.127:9000/oauth2/getRedirect").then((res)=>{
-                authURL = res;
-                setComponentState("authPrompt");
-            }).catch((err)=>{
-                console.error("Error:", err);
-                setComponentState("error");
-            })
-        }   
-    }, []);
+    setComponentState("loading");
 
-    switch(componentState){
-        case "error":
-            return(
-                <PanelComponent title="Calendar">
-                    <Error />
-                </PanelComponent>
-            );
-        case "authPrompt":
-            return(
-                <PanelComponent title="calendar">
-                    <AuthPrompt url={authURL} />
-                </PanelComponent>
-            );
-        case "displayEvents":
-            return(
-                <div id="calendar">
-                    {/* list of events */}
-                    events go here
-                </div>
-            )
+    if (cookies.get("googleAuthToken")) {
+      setComponentState("loading");
+      fetch("https://192.168.1.127:9000/calendar")
+        .then((res) => {
+          return res.json();
+        }).then((resJson)=>{
+          setCalendarData(resJson);
+          setComponentState("displayEvents");
+        })
+        .catch((err) => {
+          console.error("Error:", err);
+          setComponentState("error");
+        });
+    } else {
+      fetch("https://192.168.1.127:9000/oauth2/getRedirect")
+        .then((res) => {
+          return res.text();
+        })
+        .then((parsed) => {
+          console.log(parsed);
+          setAuthURL(parsed);
+          setComponentState("authPrompt");
+        })
+        .catch((err) => {
+          console.error("Error:", err);
+          setComponentState("error");
+        });
     }
-    
-    return(
-        <PanelComponent title="Calendar">
-            <p>Loading...</p>
-        </PanelComponent>
-    )
+  }, []);
 
+  switch (componentState) {
+    case "loading":
+      return (
+        <PanelComponent title="Calendar">
+          <p>Loading...</p>
+        </PanelComponent>
+      );
+    case "error":
+      return (
+        <PanelComponent title="Calendar">
+          <Error />
+        </PanelComponent>
+      );
+    case "authPrompt":
+      return (
+        <PanelComponent title="calendar">
+          <AuthPrompt url={authURL} />
+        </PanelComponent>
+      );
+    case "displayEvents":
+      console.log("eventData:", calendarData);
+      return (
+        <PanelComponent>
+          {"Events go here"}
+          <div id="calendar">
+            {/* list of events */}
+            
+          </div>
+        </PanelComponent>
+      );
+  }
+
+  return (
+    <PanelComponent title="Calendar">
+      <p>Loading...</p>
+    </PanelComponent>
+  );
 }
