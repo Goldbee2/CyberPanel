48a4064f163e82b0434f1e8e94d69e85f363dc0c
Eli Goldberg <goldbee2@wwu.edu>
2023-03-07 18:36:53 -0800

    Added postAuth route and global scope oauth client

--- backend/routes/oauth2.js
+++ backend/routes/oauth2.js
@@ -3,15 +3,26 @@ var router = express.Router();
 var oauth2 = require("../oAuth2");
 
 router.get("/getRedirect", function (req, res, next) {
-  let a = oauth2.getRedirectURL();
+  const oauth2Client = req.app.get("oauth2Client");
+  let a = oauth2.getRedirectURL(oauth2Client);
+  res.set("Content-Type", "text/html");
   res.send(a);
-//   a.then((b) => {
-//     console.log(b);
-//     res.send(b);
-//   });
-//   res.setHeader("content-type", "application/json");
 });
 
+router.get("/postAuth", function (req, res, next) {
 
+  const code = req.query.code;
+  const oauth2Client = req.app.get("oauth2Client");
+  let tokenExchangeStatus = oauth2.exchangeToken(oauth2Client, code);
+  tokenExchangeStatus.then((status) => {
+    res.status(status ? 200 : 400);
+    let token=oauth2Client.credentials.access_token;
+    console.log(token);
+    let url = "https://localhost:3000/authRedirect?googleAuthToken="+token; 
+    console.log(url);
+    res.redirect(url);
+  });
+  
+});
 
 module.exports = router;
