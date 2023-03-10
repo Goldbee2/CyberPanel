4e302d126ee982c5d6f4c3e23d0ab7353f0c08ca
Eli Goldberg <goldbee2@wwu.edu>
2023-03-09 22:05:54 -0800

    Added redirect and better cookie policy

--- frontend/src/AuthRedirect.js
+++ frontend/src/AuthRedirect.js
@@ -1,15 +1,26 @@
 import React from "react";
-import Cookies from 'universal-cookie';
-
+import Cookies from "universal-cookie";
+import { Navigate } from "react-router-dom";
 
 // "Needs: success state, failed state, store cookies on success, prompt user to close window."
 
+export default function AuthRedirect() {
+  const queryParameters = new URLSearchParams(window.location.search);
+  const cookies = new Cookies();
+  cookies.set("googleAuthToken", queryParameters.get("googleAuthToken"), {
+    secure: true,
+    sameSite: "Strict",
+  });
+  console.log(queryParameters);
 
-export default function AuthRedirect(){
-    const queryParameters = new URLSearchParams(window.location.search);
-    const cookies = new Cookies();
-    cookies.set('googleAuthToken', queryParameters.get('googleAuthToken'), {secure:true, sameSite:"Strict"});
-    console.log(queryParameters);
-
-    return(<p>This page should redirect you. If it doesn't, click <a href="https://localhost:3000">here</a> to be redirected.</p>)
+  return (
+    <>
+      <Navigate to="/" />
+      <p>
+        This page should redirect you. If it doesn't, click{" "}
+        <a href="https://192.168.1.127:3000">here</a> to be redirected.
+      </p>
+      
+    </>
+  );
 }
