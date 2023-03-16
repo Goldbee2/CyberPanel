db686a394f7caeebaa2a873a11c952a97e98e25b
Eli Goldberg <goldbee2@wwu.edu>
2023-03-16 13:48:35 -0700

    Added button id and removed log

--- frontend/src/components/AuthPrompt.js
+++ frontend/src/components/AuthPrompt.js
@@ -3,9 +3,8 @@ import React from "react";
 
 export default function AuthPrompt(props){
 
-    console.log(props);
 
     return(<div className="authPrompt">
-    <a href={props.url}>Login</a>
+    <a id = "login-button" href={props.url}>Login</a>
     </div>);
 }
